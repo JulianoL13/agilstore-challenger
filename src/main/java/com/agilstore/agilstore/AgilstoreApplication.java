2d432f8fc36378f76fb93d6d0d2e55d37b27abdf
JulianoL13 <julianov1383@gmail.com>
2025-01-07 18:35:54 -0300

    Add Spring Data web support for enhanced pagination and serialization.

--- src/main/java/com/agilstore/agilstore/AgilstoreApplication.java
+++ src/main/java/com/agilstore/agilstore/AgilstoreApplication.java
@@ -2,8 +2,10 @@ package com.agilstore.agilstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class AgilstoreApplication {
 
 	public static void main(String[] args) {
