419dc832577551bec3a56596165af05581c0f2a2
JulianoL13 <julianov1383@gmail.com>
2025-01-07 19:22:34 -0300

    Add ControllerExceptionHandler to manage validation exceptions.

---
+++ src/main/java/com/agilstore/agilstore/product/controllers/handlers/ControllerExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.agilstore.agilstore.product.controllers.handlers;
+
+import com.agilstore.agilstore.genericExceptions.dtos.CustomErrorDTO;
+import com.agilstore.agilstore.genericExceptions.dtos.ValidationErrorDTO;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomErrorDTO> methodArgumentNotValidation(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationErrorDTO err = new ValidationErrorDTO(Instant.now(), status.value(), "Dados inválidos", request.getRequestURI());
+        for (FieldError f : e.getBindingResult().getFieldErrors()) {
+            err.addError(f.getField(), f.getDefaultMessage());
+        }
+        return ResponseEntity.status(status).body(err);
+    }
+}
