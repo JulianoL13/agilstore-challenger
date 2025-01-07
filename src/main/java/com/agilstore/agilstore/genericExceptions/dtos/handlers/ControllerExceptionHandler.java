ba9d8a397aab6087866494c6aa3b70d92795bd53
JulianoL13 <julianov1383@gmail.com>
2025-01-07 19:43:56 -0300

    Add handling for EntityNotFoundException in ControllerExceptionHandler.

--- src/main/java/com/agilstore/agilstore/genericExceptions/dtos/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/agilstore/agilstore/genericExceptions/dtos/handlers/ControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.agilstore.agilstore.genericExceptions.dtos.handlers;
 
 import com.agilstore.agilstore.genericExceptions.dtos.CustomErrorDTO;
+import com.agilstore.agilstore.genericExceptions.dtos.EntityNotFoundException;
 import com.agilstore.agilstore.genericExceptions.dtos.ValidationErrorDTO;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -16,10 +17,17 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<CustomErrorDTO> methodArgumentNotValidation(MethodArgumentNotValidException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
-        ValidationErrorDTO err = new ValidationErrorDTO(Instant.now(), status.value(), "Dados inválidos", request.getRequestURI());
+        ValidationErrorDTO err = new ValidationErrorDTO(Instant.now(), status.value(), "Invalid data", request.getRequestURI());
         for (FieldError f : e.getBindingResult().getFieldErrors()) {
             err.addError(f.getField(), f.getDefaultMessage());
         }
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<CustomErrorDTO> entityNotFound(EntityNotFoundException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomErrorDTO err = new CustomErrorDTO(Instant.now(), status.value(), "Entity not found", request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
 }
