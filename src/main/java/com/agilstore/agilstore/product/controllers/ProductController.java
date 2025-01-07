27fb876cbb63b11f5e489db6a3c368e9850fdb27
JulianoL13 <julianov1383@gmail.com>
2025-01-07 18:36:28 -0300

    Add CRUD operations for product management in ProductController.

--- src/main/java/com/agilstore/agilstore/product/controllers/ProductController.java
+++ src/main/java/com/agilstore/agilstore/product/controllers/ProductController.java
@@ -1,14 +1,13 @@
 package com.agilstore.agilstore.product.controllers;
 
 import com.agilstore.agilstore.product.dto.ProductCategoryDTO;
-import com.agilstore.agilstore.product.dto.ProductDTO;
 import com.agilstore.agilstore.product.services.ProductService;
-import org.apache.coyote.Response;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/products")
@@ -24,6 +23,33 @@ public class ProductController {
         ProductCategoryDTO product = productService.createProduct(dto);
         return ResponseEntity.ok(product);
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductCategoryDTO> updateProduct(@PathVariable UUID id, @RequestBody ProductCategoryDTO dto) {
+        ProductCategoryDTO product = productService.updateProduct(id, dto);
+        return ResponseEntity.ok(product);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductCategoryDTO> getProductById(@PathVariable UUID id) {
+        ProductCategoryDTO product = productService.getProductById(id);
+        return ResponseEntity.ok(product);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ProductCategoryDTO>> getProducts(@RequestParam(name = "name", defaultValue = "")
+                                                                    String name, Pageable pageable) {
+        Page<ProductCategoryDTO> products = productService.getProducts(name, pageable);
+        return ResponseEntity.ok(products);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable UUID id) {
+        productService.deleteProduct(id);
+        return ResponseEntity.noContent().build();
+    }
+
 
 
 }
