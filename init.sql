f75022da27ba5bf7c267738448c40c9c20255b92
JulianoL13 <julianov1383@gmail.com>
2025-01-07 23:54:10 -0300

    Add Docker Compose and SQL initialization for the application setup.

---
+++ init.sql
@@ -0,0 +1,6 @@
+INSERT INTO categories (name, description) VALUES
+                                               ('Electronics', 'Devices and gadgets for everyday use'),
+                                               ('Clothing', 'Apparel and accessories for all genders'),
+                                               ('Home & Kitchen', 'Furniture, appliances, and decor for your home'),
+                                               ('Sports', 'Sports equipment and outdoor gear'),
+                                               ('Toys', 'Toys for children of all ages');
