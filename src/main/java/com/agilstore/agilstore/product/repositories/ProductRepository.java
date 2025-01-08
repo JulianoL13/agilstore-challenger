bf6bbcf20f26b8686e8310246eb1308a283fa50c
JulianoL13 <julianov1383@gmail.com>
2025-01-08 05:17:02 -0300

    Refactor ProductRepository to use specifications and entity graphs, solving n+1 problem.

--- src/main/java/com/agilstore/agilstore/product/repositories/ProductRepository.java
+++ src/main/java/com/agilstore/agilstore/product/repositories/ProductRepository.java
@@ -3,15 +3,16 @@ package com.agilstore.agilstore.product.repositories;
 import com.agilstore.agilstore.product.entities.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
-public interface ProductRepository extends JpaRepository<Product, UUID> {
-@Query("SELECT obj"
-        + " FROM Product obj"
-        + " WHERE LOWER(obj.name) LIKE LOWER(CONCAT('%', :name, '%'))")
-Page<Product> searchByName(String name, Pageable pageable);
-
+public interface ProductRepository extends JpaRepository<Product, UUID>, JpaSpecificationExecutor<Product> {
+    @EntityGraph(attributePaths = "category")
+    Page<Product> findAll(Specification<Product> specification, Pageable pageable);
 }
